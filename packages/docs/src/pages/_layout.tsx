3ebf0f94dabd10649f487db3ad36bb9dc585bc2f
Zeno_ <lin1071833138@gmail.com>
2025-09-25 23:36:00 +0800

    Update: Remove Waku Config

--- packages/docs/src/pages/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from 'react';
-import { Provider } from '@/components/provider';
-import '../styles/globals.css';
-
-// 根布局组件
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return <Provider>{children}</Provider>;
-}
